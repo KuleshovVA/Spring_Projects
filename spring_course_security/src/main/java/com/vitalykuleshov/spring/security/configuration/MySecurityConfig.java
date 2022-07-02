9fcc593328d963ec8b35f023649d50a5eb13485e
Vitaly Kuleshov <87039701+KuleshovVA@users.noreply.github.com>
2022-07-03 00:05:22 +0300

    Add files via upload

---
+++ spring_course_security/src/main/java/com/vitalykuleshov/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,40 @@
+package com.vitalykuleshov.spring.security.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+////
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("vitaly").password("vitaly").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+//                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+//
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+}
---
+++ spring_course_security/src/main/java/com/vitalykuleshov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.vitalykuleshov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
